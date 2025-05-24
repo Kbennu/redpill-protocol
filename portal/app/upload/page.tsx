d601bbd2f8a607f0a25e11770ea9d680b7863bc2
Kseniya Bennu <kseniya.bennu@gmail.com>
2025-05-25 01:34:58 +0300

    Update page.tsx

--- portal/app/upload/page.tsx
+++ portal/app/upload/page.tsx
@@ -1,4 +1,4 @@
-import { UploadMarkdownForm } from "@/components/forms/UploadMarkdownForm"
+import { UploadMarkdownForm } from "../components/UploadMarkdownForm"
 
 export default function Page() {
   return (
