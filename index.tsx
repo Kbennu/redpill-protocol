110ec7b909774f26e1da543f77b6759581f9eac0
Kseniya Bennu <kseniya.bennu@gmail.com>
2025-05-24 19:39:15 +0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-center">🕯️ Ты можешь войти. Или ещё нет.</h1>
+    </div>
+  );
+}
